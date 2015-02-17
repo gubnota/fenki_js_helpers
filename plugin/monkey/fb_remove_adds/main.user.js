d01214c9693567666d463ebf4cdde936f22365f2
gubnota <me@gubnota.ru>
2015-02-17 12:20:41 +0700

    rearrangment

--- plugin/monkey/fb_remove_adds/main.user.js
+++ plugin/monkey/fb_remove_adds/main.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name       Facebook remove adds
 // @namespace  http://gubnota.github.io/fenki_js_helpers/plugin/monkey/fb_remove_adds/
-// @version    0.1.1
+// @version    0.1.2
 // @downloadURL http://gubnota.github.io/fenki_js_helpers/plugin/monkey/fb_remove_adds/main.user.js
-// @updateURL http://gubnota.github.io/fenki_js_helpers/plugin/monkey/fb_remove_adds/main.meta.js
 // @description removes adds block in Facebook
 // @include      https://www.facebook.com/*
 // @copyright  2012+, Vladislav Muravyev
