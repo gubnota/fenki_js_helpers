40f0ab80ca41b0c7e816d98b696ec1bb35757f1a
gubnota <me@gubnota.ru>
2015-02-21 22:59:04 +0700

    youtube

--- plugin/monkey/youtube_download_links/window_helper.dev.js
+++ plugin/monkey/youtube_download_links/window_helper.dev.js
@@ -5,17 +5,17 @@ replaceAll = function(input, stringToFind, stringToReplaceWith) {
 setDiv = function(videos) {
     var title = 'saved video';
     var titleH1 = document.getElementById('watch-headline-title');
-    if (titleH1 != null) {
+    if (titleH1 !== null) {
         title = titleH1.children[0].innerText;
     }
     var html = '<div id="download-youtube-chrome-extension" style="-moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 3px; border: 1px solid #CCC; margin-bottom: 10px; background-color: #fff;">';
     html = html + '<div style="font-weight: bold; padding: 5px; border-bottom: 1px solid #CCC;">Click on the format to save the video as:</div>';
     html = html + '<div style="padding: 5px; font-weight: bold;">';
     var counter = 0;
-    for (i in videos) {
+    for (var i in videos) {
         var video = videos[i];
-        if (video.url != '' && video.url.indexOf('http') == 0) {
-            if (counter != 0) html = html + ' | ';
+        if (video.url !== '' && video.url.indexOf('http') === 0) {
+            if (counter !== 0) html = html + ' | ';
             if (typeof video.formatObject == 'undefined') {
                 html = html + '<span><a href="' + video.url + '&title=' + replaceAll(title, '"', '%22') + '" onclick="_gaq.push([\'_trackEvent\', \'Download\', \'' + replaceAll(replaceAll(title, '"', '&quot;'), '\'', '\\\'') + '\', \'Unknown Format\']);">Unknown Format</a></span>';
             } else {
@@ -32,22 +32,22 @@ setDiv = function(videos) {
     html = html + '<div id="download-youtube-chrome-ad-620-60"><IFRAME SRC="https://ib.adnxs.com/tt?id=3990674&referrer=[REFERRER_URL]" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="468" HEIGHT="60"></IFRAME></div>';
     html = html + '</div>';
     html = html + '<div style="padding: 5px;">';
-    html = html + 'By <a href="//www.downloadyoutubechrome.com/">Download Youtube Chrome</a> version 2.0 (<a href="//www.downloadyoutubechrome.com/latest-version.php">Check New Version</a>)';
-    html = html + ' | <a href="//www.downloadyoutubechrome.com/feedback.php">Feedback</a>';
-    html = html + ' | <a href="//www.downloadyoutubechrome.com/video-player.php">Download Player</a>';
+    // html = html + 'By <a href="//www.downloadyoutubechrome.com/">Download Youtube Chrome</a> version 2.0 (<a href="//www.downloadyoutubechrome.com/latest-version.php">Check New Version</a>)';
+    // html = html + ' | <a href="//www.downloadyoutubechrome.com/feedback.php">Feedback</a>';
+    // html = html + ' | <a href="//www.downloadyoutubechrome.com/video-player.php">Download Player</a>';
     html = html + '</div>';
     html = html + '</div>';
     var wpDiv = document.getElementById('watch7-content');
-    if (wpDiv != null) {
+    if (wpDiv !== null) {
         wpDiv.innerHTML = html + wpDiv.innerHTML;
         var sideAdDiv = document.getElementById('watch7-sidebar-contents');
-        if (sideAdDiv != null) {
+        if (sideAdDiv !== null) {
             //var html = '<iframe src="https://ad131m.adk2.co/ad131m/scripts/direct/direct.html?a=11841112&secure=true&size=300x250&ci=1&serverdomain=s.ad131m.com&context=c55452061" height="250" width="300" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
             //sideAdDiv.innerHTML = html + sideAdDiv.innerHTML;
         }
     }
     _gaq.push(['_trackPageview']);
-}
+};
 getVideos = function() {
     try {
         var formats = {
@@ -165,27 +165,27 @@ getVideos = function() {
         var videos = new Array();
         var flashVarsString = ytplayer.config.args.url_encoded_fmt_stream_map;
         var streamFiles = flashVarsString.split(',');
-        for (i in streamFiles) {
+        for (var i in streamFiles) {
             streamData = streamFiles[i].split('&');
             var url = '';
             var sig = '';
             var itag = 0;
-            for (y in streamData) {
-                if (streamData[y].indexOf('itag=') == 0) {
+            for (var y in streamData) {
+                if (streamData[y].indexOf('itag=') === 0) {
                     itagData = streamData[y].split('=');
                     itag = itagData[1];
                 }
-                if (streamData[y].indexOf('url=') == 0) {
+                if (streamData[y].indexOf('url=') === 0) {
                     urlData = streamData[y].split('=');
                     url = unescape(urlData[1]);
                 }
-                if (streamData[y].indexOf('sig=') == 0) {
+                if (streamData[y].indexOf('sig=') === 0) {
                     sigData = streamData[y].split('=');
                     sig = unescape(sigData[1]);
                 }
 
             }
-            if (url != '' && itag != 0) {
+            if (url !== '' && itag !== 0) {
                 var video = {
                     formatObject: formats[itag],
                     url: url + '&signature=' + sig
@@ -203,11 +203,11 @@ getVideos = function() {
 
 listener = function() {
     var ext = document.getElementById('download-youtube-chrome-extension');
-    if (typeof ytplayer != 'undefined' &&
-        typeof ytplayer.config != 'undefined' &&
-        ytplayer.config != null &&
-        typeof ytplayer.config.args != 'undefined' &&
-        typeof ytplayer.config.args.url_encoded_fmt_stream_map != 'undefined' &&
+    if (typeof ytplayer !== 'undefined' &&
+        typeof ytplayer.config !== 'undefined' &&
+        ytplayer.config !== null &&
+        typeof ytplayer.config.args !== 'undefined' &&
+        typeof ytplayer.config.args.url_encoded_fmt_stream_map !== 'undefined' &&
         ext == null) {
         setDiv(getVideos());
     }
