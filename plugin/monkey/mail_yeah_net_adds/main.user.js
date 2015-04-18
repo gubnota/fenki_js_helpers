ef6d2af5b027fdda5bc7b57fc5dafe1803b65d55
gubnota <me@gubnota.ru>
2015-04-18 10:39:20 +0800

    yeah_net_adds

--- plugin/monkey/mail_yeah_net_adds/main.user.js
+++ plugin/monkey/mail_yeah_net_adds/main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       mail.yeah.net adds remover
 // @namespace  http://gubnota.github.io/fenki_js_helpers/plugin/monkey/mail_yeah_net_adds/
-// @version    0.1.1
+// @version    0.1.2
 // @downloadURL http://gubnota.github.io/fenki_js_helpers/plugin/monkey/mail_yeah_net_adds/main.user.js
 // @description removes adds block(s) in mail.yeah.net
 // @include      http://mail.yeah.net/*
@@ -13,7 +13,7 @@
 // ==/UserScript==
 (function(){
 var t = setInterval(function(){
-var el = document.querySelectorAll('.xd0,.gWel-promt,.gWel-tabs-promt,.gWel-tabs-panel,.gWel-tabs-panel-hasBg,#_mail_tabitem_2_34');
+var el = document.querySelectorAll('.xd0,.gWel-promt,.gWel-tabs-promt,.gWel-tabs-panel,.gWel-tabs-panel-hasBg,#_mail_tabitem_2_34,.js-component-popup');
     for(var i =0; i<el.length; i++){
     el[i].remove();//clearInterval(t);
     }
