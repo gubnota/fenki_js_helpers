3da1e62493a02809f4de530027d7b7d33cc37209
gubnota <me@gubnota.ru>
2015-05-28 22:38:02 +0800

    thumb url minor change; "Undefined" internationalization

--- plugin/monkey/shutter_places/window_helper.dev.js
+++ plugin/monkey/shutter_places/window_helper.dev.js
@@ -4,8 +4,7 @@
  Version 2.0 (the "License"); you may not use this file except in 
  compliance with the License. You may obtain a copy of the License at
 
- http://www.ap реезЖ//цццюфзфсрующкп/дшсутыуы/ДШСУТЫУ-2ю0
-ache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
