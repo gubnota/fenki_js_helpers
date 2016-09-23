34603589670f3d4486aa8a6a767af9441d3e9d54
gubnota <me@gubnota.ru>
2016-09-23 18:55:42 +0800

    mail.yandex.ru imgs

--- plugin/monkey/mail_yandex_ru_adds/hide_img.user.js
+++ plugin/monkey/mail_yandex_ru_adds/hide_img.user.js
@@ -19,7 +19,7 @@ empty_dot="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAAAAAAALAAAAAABAAE
 click_handlers='.ns-view-messages-item',/*on click which items to (re)-run hide images*/
 insert_button_place='.ns-view-message-toolbar',/*where to insert show images button*/
 button_id='show_img',/*button id to show images*/
-button_code='<span class="mail-Message-Toolbar-Item"><span id="show_img" class="mail-Message-Toolbar-Item-Text" style="cursor:pointer;">Показать картинки</span></span>',
+button_code='<span id="show_img" style="display:inline-block;cursor:pointer;">Показать картинки</span>',
 button_where='afterBegin',
 button_from='Показ',
 button_to='Спрят',
