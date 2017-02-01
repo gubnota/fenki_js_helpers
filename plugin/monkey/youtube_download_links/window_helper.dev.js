c472e436f8f8c347767edf8c129d0f0c4029c3ec
gubnota <me@gubnota.ru>
2017-02-01 23:55:55 +0800

    youtube

--- plugin/monkey/youtube_download_links/window_helper.dev.js
+++ plugin/monkey/youtube_download_links/window_helper.dev.js
@@ -163,31 +163,31 @@ var button_click_event_handler_activator = function(){
         var clickEventHandler2 = function (event) {
             event.preventDefault();
             var link = event.target;
-            link.download = link.getAttribute('title');
-            link.click();
-            // var xhr = new XMLHttpRequest();
-            // xhr.open("GET", requestUrl);
-            // xhr.responseType = "blob";
-            // xhr.onload = function () {
-            //     if (this.status === 200) {
-            //         var blob = new Blob([this.response], {type: "video/mp4"});//
-            //         //Create a link element, hide it, direct
-            //         //it towards the blob, and then 'click' it programatically
-            //         let a = document.createElement("a");
-            //         a.style = "display: none";
-            //         document.body.appendChild(a);
-            //         //Create a DOMString representing the blob
-            //         //and point the link element towards it
-            //         let url = window.URL.createObjectURL(blob);
-            //         a.href = url;
-            //         a.download = 'Love.mp4';
-            //         //programatically click the link to trigger the download
-            //         a.click();
-            //         //release the reference to the file by revoking the Object URL
-            //         window.URL.revokeObjectURL(url);
-            //     }
-            // };
-            // xhr.send();
+            var download = link.getAttribute('title');
+            var requestUrl = link.getAttribute('href');
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", requestUrl);
+            xhr.responseType = "blob";
+            xhr.onload = function () {
+                if (this.status === 200) {
+                    var blob = new Blob([this.response], {type: "video/mp4"});//
+                    //Create a link element, hide it, direct
+                    //it towards the blob, and then 'click' it programatically
+                    var a = document.createElement("a");
+                    a.style = "display: none";
+                    document.body.appendChild(a);
+                    //Create a DOMString representing the blob
+                    //and point the link element towards it
+                    var url = window.URL.createObjectURL(blob);
+                    a.href = url;
+                    a.download = download;
+                    //programatically click the link to trigger the download
+                    a.click();
+                    //release the reference to the file by revoking the Object URL
+                    window.URL.revokeObjectURL(url);
+                }
+            };
+            xhr.send();
         }
         el.onclick = clickEventHandler2;
         if(TouchEvent) el.ontouchstart = clickEventHandler2;
