8f4395ec64b36a8a9edba20851be0bf9f6afdb62
gubnota <me@gubnota.ru>
2017-02-11 09:30:32 +0800

    facebook

--- plugin/monkey/fb_remove_adds/main.user.js
+++ plugin/monkey/fb_remove_adds/main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Facebook remove adds
 // @namespace  http://gubnota.github.io/fenki_js_helpers/plugin/monkey/fb_remove_adds/
-// @version    0.1.3
+// @version    0.1.4
 // @downloadURL http://gubnota.github.io/fenki_js_helpers/plugin/monkey/fb_remove_adds/main.user.js
 // @description removes adds block in Facebook
 // @include      https://www.facebook.com/*
@@ -20,7 +20,17 @@ var el = document.getElementsByClassName('ego_column');
 
 var r = RegExp(/">Реклама<\/a>/);
 el = document.querySelectorAll('._5jmm._5pat._3lb4._59m._x72'); for(var i=0; i<el.length; i++) {
-if (r.test(el[i].innerHTML)) el[i].remove();
+if (r.test(el[i].innerHTML)) {el[i].style.visibility='hidden';el[i].style.height="1px";}
+}
+var a = document.querySelectorAll('._5jmm._5pat._3lb4.d_1earfv54ng._x72,._4ikz');
+var ads = false;
+for (var i = 0; i< a.length; i++){
+	ads = false;
+	var b = a[i].querySelectorAll('span');
+	for (var j = 0; j<b.length;j++){
+	if (b[j].innerText == 'Рекомендуемая публикация' || b[j].innerText == 'Реклама') ads=true;
+	}
+	if (ads) {a[i].style.visibility='hidden';a[i].style.height="10px";}
 }
 
     },100);
