393d34c8c221e85a98b70f1a800c8f8e11698a3f
gubnota <me@gubnota.ru>
2018-09-07 03:19:06 +0800

    forvo

---
+++ plugin/monkey/forvo/main.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name       Forvo links
+// @namespace  http://gubnota.github.io/fenki_js_helpers/plugin/monkey/forvo/
+// @version    0.0.1
+// @downloadURL http://gubnota.github.io/fenki_js_helpers/plugin/monkey/forvo/main.user.js
+// @description Forvo links
+// @include        http://*.forvo.com/*
+// @include        https://*.forvo.com/*
+// @include        http://forvo.com/*
+// @include        https://forvo.com/*
+// @copyright  2018+, Larry Moore
+// @author Larry Moore
+// @grant          none
+// @run-at document-end
+// ==/UserScript==
+(function(window, undefined){
+var w = window;
+    // без этого условия скрипт будет запускаться несколько раз на странице с фреймами
+    if (w.self != w.top) {
+        return;
+    }
+var a = document.getElementsByClassName('play');
+for (var i = a.length - 1; i >= 0; i--) {
+var b = a[i].getAttribute('onclick');
+if (/^PlayPhrase\(/.test(a[i]))
+    {
+    var g = /'([^\']+)'/.exec(a[i]);
+if (g[1] !== undefined) 
+    {
+       a[i].setAttribute('onclick','//audio.forvo.com/mp3/'+atob(g[1]));
+    }
+
+    }
+else if (/^Play\(/.test(a[i])) {
+    var g = /'([^\']+)'/.exec(a[i]);
+if (g[1] !== undefined) 
+    {
+       a[i].setAttribute('onclick','//audio00.forvo.com/phrases/mp3/'+atob(g[1]));
+    }
+    }
+}
+
+})(window);
