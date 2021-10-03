94b8f0a53303db9b879d912263c877697ec4b492
gubnota <hvw@yandex.com>
2021-10-03 12:09:35 +0800

    console.save

---
+++ plugin/monkey/console.save/main.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         console.save as file
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  saves json objects to file when one calls console.save(data)
+// @author       You
+// @match        http://localhost:*/*
+// @match        http://127.0.0.1:*/*
+// @match        http://192.168.*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// ==/UserScript==
+// Usage: console.save('content', 'filename')
+(function(){
+  window.console.save = (data, filename)=>{
+  
+      if(!data) {
+          console.error('Console.save: No data')
+          return;
+      }
+      if(!filename) filename = 'console.json'
+  
+      if(typeof data === "object"){
+          data = JSON.stringify(data, undefined, 4)
+      }
+  
+      const blob = new Blob([data], {type: 'text/json'});
+      const e = document.createEvent('MouseEvents');
+      const a = document.createElement('a');
+  
+      a.download = filename
+      a.href = window.URL.createObjectURL(blob)
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+      e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+      a.dispatchEvent(e)
+      setTimeout(()=>{window.URL.revokeObjectURL(a.href); delete a},1000);
+   }
+  })()
